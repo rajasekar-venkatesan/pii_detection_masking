a467a45621f80843aa5e1202f42ca7b60570cd00
Rajasekar Venkatesan <raja0046@e.ntu.edu.sg>
2021-04-17 20:30:59 +0530

    Updated default text

--- host_streamlit.py
+++ host_streamlit.py
@@ -13,7 +13,7 @@ from pdm import pdm_model, ENTITIES_OF_INTEREST
 
 ## PII Entities
 """
-text = "My name is Rajasekar Venkatesan and my phone number is +6597720584. I live in Singapore but I am from Tamil Nadu, India. I'm an Indian and Hindu. My GPU machine's local address is 192.162.18.9 and my email address is rajasekar_venkatesan@singaporeair.com.sg. I'm working on a search project for www.singaporeair.com.sg for about 2 months 12 days and 6 hours as of 04:30 PM today. My credit card number is 4119-1101-0319-4913 and my FIN number is G1162041N"
+text = "My name is Rajasekar Venkatesan and my phone number is +6597720584. I live in Singapore but I am from Tamil Nadu, India. I'm an Indian and Hindu. My GPU machine's local address is 192.162.18.9 and my email address is rajasekar_venkatesan@singaporeair.com.sg. I'm working on a search project for www.singaporeair.com.sg for about 2 months 12 days and 6 hours as of 04:30 PM today. My credit card number is 4119-1101-0319-4913 and my FIN number is G1162041N. Fake credit card number is 4123-4567-8901-2345 and fake FIN number is G1234567N"
 entities_of_interest = st.multiselect("Select the entities to be detected: ", ENTITIES_OF_INTEREST, ['PERSON'])
 text = st.text_area("Enter text: ", text)
 result = pdm_model.predict(text, entities_of_interest)
